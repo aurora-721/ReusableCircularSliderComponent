2ce6b0c3e7d1efc4655452aee33c0b79ae3d7e3b
Hausos <aurora.makovac@gmail.com>
2020-12-14 15:58:13 +0100

    adding options component

--- circularSlider.js
+++ circularSlider.js
@@ -14,17 +14,24 @@ template.innerHTML = `
 class CircularSlider extends HTMLElement {
     constructor() {
         super();
-        this.attachShadow({mode: 'open'})
+        this.attachShadow({mode: 'open'});
         this.shadowRoot.appendChild(template.content.cloneNode(true));
 
-        this.radius = 0;
+        this.options = {
+            container: 0,
+            color: '#000',
+            minVal: 0,
+            maxVal: 100,
+            step: 1,
+            radius: 0
+        };
     }
 
     changePosition() {
         const circle = this.shadowRoot.querySelector('.circle');
         let deg = 20;
 
-        circle.style.transform = "rotate(" + deg + "deg) translate(" + this.radius * Math.cos(deg*Math.PI/180) + "em," +  this.radius * Math.sin(deg*Math.PI/180) +"em)";
+        circle.style.transform = "rotate(" + deg + "deg) translate(" + this.options.radius * Math.cos(deg*Math.PI/180) + "em," +  this.options.radius * Math.sin(deg*Math.PI/180) +"em)";
         
     }
 
@@ -37,16 +44,15 @@ class CircularSlider extends HTMLElement {
 
         //see if attribute is defined
         if (this.getAttribute('radius')) {
-            this.radius = this.getAttribute('radius');
+            this.options.radius = this.getAttribute('radius');
             let deg = 0;
             
-            console.log(this.radius)
             // defining the radius of the circle
-            this.shadowRoot.querySelector('.slider').style.width = 2 * this.radius + "em";
-            this.shadowRoot.querySelector('.slider').style.height = 2 * this.radius + "em";
+            this.shadowRoot.querySelector('.slider').style.width = 2 * this.options.radius + "em";
+            this.shadowRoot.querySelector('.slider').style.height = 2 * this.options.radius + "em";
             // defining the position of the circle
             const circle = this.shadowRoot.querySelector('.circle');
-            circle.style.transform = "rotate(" + deg + "deg) translate(" + this.radius * Math.cos(deg*Math.PI/180) + "em," +  this.radius * Math.sin(deg*Math.PI/180) +"em)";
+            circle.style.transform = "rotate(" + deg + "deg) translate(" + this.options.radius * Math.cos(deg*Math.PI/180) + "em," +  this.options.radius * Math.sin(deg*Math.PI/180) +"em)";
 
         }
 
