68d278898f05763c48bd591f89da39b65569754f
Hausos <aurora.makovac@gmail.com>
2020-12-14 16:22:39 +0100

    config option components

--- circularSlider.js
+++ circularSlider.js
@@ -23,13 +23,13 @@ class CircularSlider extends HTMLElement {
             minVal: 0,
             maxVal: 100,
             step: 1,
-            radius: 0
+            radius: 10
         };
     }
 
     changePosition() {
         const circle = this.shadowRoot.querySelector('.circle');
-        let deg = 20;
+        let deg = 45;
 
         circle.style.transform = "rotate(" + deg + "deg) translate(" + this.options.radius * Math.cos(deg*Math.PI/180) + "em," +  this.options.radius * Math.sin(deg*Math.PI/180) +"em)";
         
@@ -42,19 +42,38 @@ class CircularSlider extends HTMLElement {
             this.changePosition()
         })
 
-        //see if attribute is defined
+
+        //extract the defined attributes
+        if (this.getAttribute('color')) {
+            this.options.color = this.getAttribute('color');
+        }
+        if (this.getAttribute('container')) {
+            this.options.container = this.getAttribute('container');
+        }
+        if (this.getAttribute('maxVal')) {
+            this.options.maxVal = this.getAttribute('maxVal');
+        }
+        if (this.getAttribute('minVal')) {
+            this.options.minVal = this.getAttribute('minVal');
+        }
         if (this.getAttribute('radius')) {
             this.options.radius = this.getAttribute('radius');
-            let deg = 0;
-            
-            // defining the radius of the circle
-            this.shadowRoot.querySelector('.slider').style.width = 2 * this.options.radius + "em";
-            this.shadowRoot.querySelector('.slider').style.height = 2 * this.options.radius + "em";
-            // defining the position of the circle
-            const circle = this.shadowRoot.querySelector('.circle');
-            circle.style.transform = "rotate(" + deg + "deg) translate(" + this.options.radius * Math.cos(deg*Math.PI/180) + "em," +  this.options.radius * Math.sin(deg*Math.PI/180) +"em)";
-
         }
+        if (this.getAttribute('step')) {
+            this.options.step = this.getAttribute('step');
+        }
+
+        let deg = 0;
+        
+        // defining the radius of the circle
+        this.shadowRoot.querySelector('.slider').style.width = 2 * this.options.radius + "em";
+        this.shadowRoot.querySelector('.slider').style.height = 2 * this.options.radius + "em";
+        // defining the position of the circle
+        const circle = this.shadowRoot.querySelector('.circle');
+        circle.style.transform = "rotate(" + deg + "deg) translate(" + this.options.radius * Math.cos(deg*Math.PI/180) + "em," +  this.options.radius * Math.sin(deg*Math.PI/180) +"em)";
+        
+
+        console.log(this.options);
 
     }
 }
