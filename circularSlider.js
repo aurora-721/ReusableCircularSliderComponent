a3546cecf3287f2f0481715eb2db4df69dea1aec
Hausos <aurora.makovac@gmail.com>
2020-12-14 15:20:45 +0100

    changing radius

--- circularSlider.js
+++ circularSlider.js
@@ -16,20 +16,40 @@ class CircularSlider extends HTMLElement {
         super();
         this.attachShadow({mode: 'open'})
         this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+        this.radius = 0;
     }
 
     changePosition() {
         const circle = this.shadowRoot.querySelector('.circle');
-        let position = 20;
+        let deg = 20;
 
-        circle.style.transform = "rotate(" + position + "deg) translate(20px, 20px)";
+        circle.style.transform = "rotate(" + deg + "deg) translate(" + this.radius * Math.cos(deg*Math.PI/180) + "em," +  this.radius * Math.sin(deg*Math.PI/180) +"em)";
         
     }
+
+    
 
     connectedCallback() {
         this.shadowRoot.querySelector('.circle').addEventListener('click', () => {
             this.changePosition()
         })
+
+        //see if attribute is defined
+        if (this.getAttribute('radius')) {
+            this.radius = this.getAttribute('radius');
+            let deg = 0;
+            
+            console.log(this.radius)
+            // defining the radius of the circle
+            this.shadowRoot.querySelector('.slider').style.width = 2 * this.radius + "em";
+            this.shadowRoot.querySelector('.slider').style.height = 2 * this.radius + "em";
+            // defining the position of the circle
+            const circle = this.shadowRoot.querySelector('.circle');
+            circle.style.transform = "rotate(" + deg + "deg) translate(" + this.radius * Math.cos(deg*Math.PI/180) + "em," +  this.radius * Math.sin(deg*Math.PI/180) +"em)";
+
+        }
+
     }
 }
 
