56cbfd36741ec837b7b86232146a43769a7fabed
Hausos <aurora.makovac@gmail.com>
2020-12-24 06:24:35 +0100

    Integrating code with babel and webpack

---
+++ README.md
@@ -0,0 +1,39 @@
+# Javascript circular slider component
+
+### Version
+
+1.1.0
+
+### Installation
+
+Install the dependencies
+
+```sh
+$ npm install
+```
+
+### Serve
+
+To serve in the browser - Runs webpack-dev-server
+
+```sh
+$ npm start
+```
+
+### Build
+
+Compile and build
+
+```sh
+$ npm run build
+```
+
+## More Info
+
+### Author
+
+Aurora Makovac
+
+### License
+
+This project is licensed under the MIT License
--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const template = document.createElement('template');
 template.innerHTML = `
-    <link rel="stylesheet" href="./styles/circularSlider.css" />
+    <link rel="stylesheet" href="../styles/circularSlider.css" />
         <svg xmlns="http://www.w3.org/2000/svg" id="circularSliderRoot" width="700" height="700" viewBox="-200 -200 400 400">
             <circle cx="0" cy="0" r="100" fill="none" class="dashed-circle" transform="rotate(-90)" style="stroke-width: 20px; stroke-dasharray: 5, 2;"></circle>
             <circle cx="0" cy="0" r="100" fill="none" class="circle-path" transform="rotate(-90)" stroke-dasharray="110 628.3185307179587" stroke-dashoffset="0" style="stroke: rgb(93, 59, 109); stroke-width: 20px;"></circle>
