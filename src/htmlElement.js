62fb303da6d804a5354ef5a7970054f71b9b660c
Hausos <aurora.makovac@gmail.com>
2020-12-25 16:29:31 +0100

    added multiple slider components

--- src/htmlElement.js
+++ src/htmlElement.js
@@ -1,155 +1,123 @@
-import {template} from './template';
+import {
+    calculateClosestDeg,
+    calculatePathLength,
+    calculateMousePosition
+  } from './calculateCircleParts';
 
 
 export class CircularSlider extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({mode: 'open'});
-        this.shadowRoot.appendChild(template.content.cloneNode(true));
+        
+        // the options component
 
-        this.options = {
+        this.presets = [
+        {
             container: 0,
-            color: '#000',
-            minVal: 0,
-            maxVal: 100,
-            step: 1,
-            radius: 100
-        };
-
+            color: '#005',
+            minVal: 1,
+            maxVal: 220,
+            step: 50,
+            radius: 50
+        }];
+        
         this.circleConf = {
             radius: 12,
         };
+        
+        this.getAttributesFromHtml();
 
-        this.circleClicked = false;
+        this.loadSliders();
+     
 
-        this.initCircularSlider();
+        this.circleClicked = [false, false, false]; //important for events
+        this.svg = this.shadowRoot.getElementById('circularSliderRoot');
 
     }
 
-    changePosOnMouseOver(e, circle, path) {
-        let deg = this.calculateMousePosition(e);
+    loadSliders() {
+        const template = document.createElement('template');
+
+        let input = ``;
+        input = `
+            <link rel="stylesheet" href="../styles/circularSlider.css" />
+                <svg xmlns="http://www.w3.org/2000/svg" id="circularSliderRoot" width="700" height="700" viewBox="-200 -200 400 400">`;
+        for (let i = 0; i < this.presets.length; i++) {
+            input += `<circle cx="0" cy="0" r=${this.presets[i].radius} fill="none" class="dashed-circle" transform="rotate(-90)" style="stroke-width: 20px; stroke-dasharray: 5, 2;"></circle>
+                    <circle cx="0" cy="0" r=${this.presets[i].radius} fill="none" class="circle-path" transform="rotate(-90)" stroke-dasharray="110 628.3185307179587" stroke-dashoffset="0" style="stroke: ${this.presets[i].color}; stroke-width: 20px;"></circle>
+                    <circle cx="0" cy="0" r=${this.presets[i].radius} fill="none" class="invisible-layer" style="stroke-width: 20px; stroke: transparent;"></circle>
+                    <circle cx="0" cy=${-this.presets[i].radius} r=${this.circleConf.radius} fill="#fff" class="small-circle" style="transform: rotate(180deg); transition: all 0.5s ease-in-out 0s;"></circle>`;
+        }
+        input += ` </svg>
+                <slot name="individualSlider"/>`;
+
+        template.innerHTML = input;
+
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+           
+    }
+
+    changePosOnMouseOver(e, circle, path, options) {
+        let deg = calculateMousePosition(e, this.svg);
 
         circle.style.transform = "rotate(" + deg + "deg)";
 
-        path.style.strokeDasharray = this.calculatePathLength(deg);
+        path.style.strokeDasharray = calculatePathLength(deg, options);
     }
 
-    calculateMousePosition(e) {
-        //relative position of cursor calculated, circle radius is subtracted so that the measurement is from the center
-        // e.clientX - position of the mouse on the screen
-        
-        let svg = this.shadowRoot.getElementById('circularSliderRoot');
-        let CTM = svg.getScreenCTM();
-        let dragX = (e.clientX - CTM.e) / CTM.a;
-        let dragY = (e.clientY - CTM.f) / CTM.d;
-        let mPos = {x: dragX, y: dragY};
 
-        //returns string that can be used to rotate the circle
-        let atan = Math.atan2(mPos.x, mPos.y);
-        let deg = -atan/(Math.PI/180) + 180;
-        
-        return deg;
 
-    }
-
-    animateToFixedPosition(e, circle, path) {
-        let mouseDeg = this.calculateMousePosition(e);
-        let deg = this.calculateClosestDeg(mouseDeg);
+    animateToFixedPosition(e, circle, path, options) {
+        let mouseDeg = calculateMousePosition(e, this.svg);
+        let deg = calculateClosestDeg(mouseDeg, options);
 
         circle.style.transform = "rotate("+ deg + "deg)";
         circle.style.transition =  "transform 0.4s cubic-bezier(0.33, 1, 0.68, 1)";
 
-        path.style.strokeDasharray = this.calculatePathLength(deg);
+        path.style.strokeDasharray = calculatePathLength(deg, options);
         path.style.transition = "stroke-dasharray 0.4s cubic-bezier(0.33, 1, 0.68, 1)";
     }
 
-    calculateClosestDeg(mouseDeg) {
-        // Takes the difference between max and min value and divides it with step - this gives the num of slices
-        let circleDivisions = (this.options.maxVal - this.options.minVal) / this.options.step;
-        // then 360 is divided with that number
-        let angleDeg = 360 / circleDivisions;
-        // rounds the angle to the nearest angle
-        let numOfAngleDeg = Math.round(mouseDeg/angleDeg);
-        let deg = angleDeg * numOfAngleDeg;
-
-        return deg;
-    }
-
-    calculatePathLength(deg) {
-        let circumference = 2 * Math.PI * this.options.radius;
-        let angle = circumference * deg / 360;
-        return angle + " " + circumference;
-    }
-    
 
     connectedCallback() {
+        const smallCircle = this.shadowRoot.querySelectorAll('.small-circle');
+        const circlePath = this.shadowRoot.querySelectorAll('.circle-path');
+        const invisibleLayer = this.shadowRoot.querySelectorAll('.invisible-layer');
         
-        
-        const smallCircle = this.shadowRoot.querySelector('.small-circle');
-        const circlePath = this.shadowRoot.querySelector('.circle-path');
-
         //mouse clicks on circle
-        this.shadowRoot.querySelector('.small-circle').addEventListener('mousedown', (e) => {
-            this.circleClicked = true;
-            
-            //when mouse pressed there are no transitions
-            smallCircle.style.transition = 'none';
-            circlePath.style.transition = 'none';
+        this.shadowRoot.querySelectorAll('.small-circle').forEach((item, index) => {
+            item.addEventListener('mousedown', (e) => {
+                this.circleClicked[index] = true;
+                
+                //when mouse pressed there are no transitions
+                item.style.transition = 'none';
+                circlePath[index].style.transition = 'none';
+            })
+            document.querySelector('html').addEventListener('mousemove', (e) => {
+                if(this.circleClicked[index]) {
+                    this.changePosOnMouseOver(e, item, circlePath[index], this.presets[index]);
+                }
+            })
+            document.querySelector('html').addEventListener('mouseup', (e) => {
+                if(this.circleClicked[index]) {
+                    this.circleClicked[index] = false;
+                    this.animateToFixedPosition(e, item, circlePath[index], this.presets[index]);
+                }
+            })
+            invisibleLayer[index].addEventListener('mouseup', (e) => {
+                if (this.circleClicked.every(elem => elem == false)) {
+                    this.animateToFixedPosition(e, item, circlePath[index], this.presets[index]);
+                }
+            })
         })
-        document.querySelector('html').addEventListener('mousemove', (e) => {
-            if(this.circleClicked) {
-                this.changePosOnMouseOver(e, smallCircle, circlePath);
-            }
-        })
-        document.querySelector('html').addEventListener('mouseup', (e) => {
-            if(this.circleClicked) {
-                this.circleClicked = false;
-                this.animateToFixedPosition(e, smallCircle, circlePath);
-            }
-        })
-        this.shadowRoot.querySelector('.invisible-layer').addEventListener('mouseup', (e) => {
-            this.animateToFixedPosition(e, smallCircle, circlePath);
-        })
-
-        
-
     }
 
-    initCircularSlider() {
+    getAttributesFromHtml() {
         //extract the defined attributes
-        if (this.getAttribute('color')) {
-            this.options.color = this.getAttribute('color');
+        if (this.getAttribute('options')) {
+            let forParsing = this.getAttribute('options');
+            this.presets = JSON.parse(forParsing);
         }
-        if (this.getAttribute('container')) {
-            this.options.container = this.getAttribute('container');
-        }
-        if (this.getAttribute('maxVal')) {
-            this.options.maxVal = this.getAttribute('maxVal');
-        }
-        if (this.getAttribute('minVal')) {
-            this.options.minVal = this.getAttribute('minVal');
-        }
-        if (this.getAttribute('radius')) {
-            this.options.radius = this.getAttribute('radius');
-        }
-        if (this.getAttribute('step')) {
-            this.options.step = this.getAttribute('step');
-        }
-
-
-
-        
-        //set default propreties for the circle
-        this.shadowRoot.querySelector('.dashed-circle').setAttribute('r', this.options.radius);
-        this.shadowRoot.querySelector('.invisible-layer').setAttribute('r', this.options.radius);
-        this.shadowRoot.querySelector('.small-circle').setAttribute('cy', -this.options.radius);
-        this.shadowRoot.querySelector('.circle-path').setAttribute('r', this.options.radius);
-        this.shadowRoot.querySelector('.small-circle').setAttribute('r', this.circleConf.radius);
-
-        this.shadowRoot.querySelector('.small-circle').style.transform = "rotate(90deg)";
-
-        this.calculatePathLength();
     }
-
-
 }
